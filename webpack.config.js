45dd19e1fb51bd869b0c17c04ed63fe24910ce85
Winnie Poon <winnie.poon.ho@gmail.com>
2018-11-08 23:44:01 +0800

    fixed clean webpack plugin removing dist/*

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     hot: true
   },
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(['dist'], {exclude: ['.gitignore']}),
     new HtmlWebpackPlugin({
       title: 'Output Management'
     }),

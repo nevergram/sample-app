bea9c1b5dbd1ddbed83eb9056572a25f54432f69
Winnie Poon <winnie.poon.ho@gmail.com>
2018-11-08 22:55:54 +0800

    file-loader

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from 'lodash';
 import './style.css';
+import Mugatu from './mugatu.png';
+
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  var myIcon = new Image();
+  myIcon.src = Mugatu;
+
+  element.appendChild(myIcon);
 
   return element;
 }

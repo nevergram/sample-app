7f4fdbe2dee1a09de779ed442d258821ddd44722
Winnie Poon <winnie.poon.ho@gmail.com>
2018-11-10 16:52:02 +0800

    webpack-merge

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import {cube} from './math.js';
 import './style.css';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
 
 function component() {
   let element = document.createElement('pre');
--- webpack.common.js
+++ webpack.common.js
@@ -1,31 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
-  // mode: 'development',
-  // optimization: {
-  //   usedExports: true
-  // },
-  mode: 'production',
   entry: {
     app: './src/index.js'
-  },
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist',
-    hot: true
   },
   plugins: [
     new CleanWebpackPlugin(['dist'], {exclude: ['.gitignore']}),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    }),
-    new webpack.HotModuleReplacementPlugin()
+      title: 'Production'
+    })
   ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
   module: {
